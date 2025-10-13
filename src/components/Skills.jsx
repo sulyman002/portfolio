dd58c7ac35ed8eb1aac0a93b255dd40975de2424
sulyman002 <oyedelesulaiman@gmail.com>
2025-10-13 05:37:38 +0100

    fix: downgrade to react 18 for compatability of some packages

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,10 +3,8 @@ import { core } from "../Data/Data";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
-
 gsap.registerPlugin(ScrollTrigger);
 const Skills = () => {
- 
   const cardsRef = useRef([]);
 
   useEffect(() => {
@@ -65,38 +63,6 @@ const Skills = () => {
                     </p>
                   </div>
                 ))}
-              </div>
-            </div>
-            <div class="min-h-screen bg-[url('/path/to/your-photo.jpg')] bg-cover bg-center flex items-center justify-center p-6">
-              {/* <!-- glass card --> */}
-              <div class="w-full max-w-md p-6 rounded-2xl bg-white/20 backdrop-blur-md border border-white/10 shadow-lg drop-shadow-lg">
-                <div class="flex items-center gap-4">
-                  <div class="w-14 h-14 rounded-full bg-white/30 backdrop-blur-sm flex items-center justify-center text-xl font-semibold">
-                    S
-                  </div>
-                  <div>
-                    <h3 class="text-white font-bold text-lg">
-                      Sulaiman Oyedele
-                    </h3>
-                    <p class="text-white/75 text-sm">
-                      Frontend Engineer • Pulsepoint Tech
-                    </p>
-                  </div>
-                </div>
-
-                <p class="mt-4 text-white/80 text-sm">
-                  Building elegant, efficient, and automated frontend
-                  experiences.
-                </p>
-
-                <div class="mt-6 flex gap-3">
-                  <button class="px-4 py-2 rounded-md bg-white/12 text-white backdrop-blur-sm border border-white/8 hover:bg-white/16 transition">
-                    Message
-                  </button>
-                  <button class="px-4 py-2 rounded-md bg-[#ec5c29] text-white font-semibold shadow-sm hover:scale-[1.02] transition-transform">
-                    Hire me
-                  </button>
-                </div>
               </div>
             </div>
           </div>
