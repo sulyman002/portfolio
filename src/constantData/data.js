589295d1c14147a2bf1a2077501c05db212d64f8
sulyman002 <oyedelesulaiman@gmail.com>
2025-10-15 14:32:18 +0100

    now done

--- src/constantData/data.js
+++ src/constantData/data.js
@@ -4,10 +4,11 @@ import myImage from "../assets/myImage.jpg"
 export const experiences = [
   {
     title: "Frontend Developer",
-    
+
+    company_name: "incite360 (crud dashboard)",
     icon: myImage,
     iconBg: "#383E56",
-    date: "March 2025 - April 2025",
+    date: "April 2025 - April 2025",
     points: [
       "Engineered a high-performance web dashboard empowering businesses to manage and secure employer profiles efficiently.",
       "Built with React, Tailwind CSS, TanStack, and React Hook Form, delivering a responsive and scalable user experience.",
@@ -16,42 +17,31 @@ export const experiences = [
     ],
   },
   {
-    title: "React Native Developer",
-    company_name: "Tesla",
+    title: "Frontend Developer",
+    company_name: "pulsepoint-mini-mart",
     icon: myImage,
     iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    date: "May 2025",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Built a responsive e-commerce platform enabling users to browse and order clothing for all genders",
+      "Developed with React, Tailwind CSS, and Redux Toolkit, ensuring fast performance and centralized state management.",
+      "Implemented dynamic product listing, cart management, and order functionality for a smooth shopping experience",
+      "Focused on efficient data handling and server communication, gaining strong insight into managing asynchronous state and API-driven workflows.",
     ],
   },
   {
-    title: "Web Developer",
-    company_name: "Shopify",
+    title: "Frontend Developer",
+    company_name: "tourvisto",
     icon: myImage,
     iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
+    date: "June - June 2025",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Developed a dynamic, responsive travel platform that enables users to plan trips and book destinations seamlessly.",
+      "Translated Figma designs into a functional frontend using React, Tailwind CSS, and Redux Toolkit to ensure scalability and smooth state management.",
+      "Integrated Google Authentication for secure and seamless user sign-in via email.",
+      "Focused on clean architecture, smooth animations, and performance optimization to enhance overall user experience.",
+      "Independently managed the frontend development, emphasizing modular code and intuitive user flow."
     ],
   },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: myImage,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
+  
 ];
